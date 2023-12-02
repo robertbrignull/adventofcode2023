ee03a29fda0e86fcdd520a6e180f8ef96f2e49ae
Robert Brignull <robertbrignull@gmail.com>
2023-12-02 10:17:48 +0000

    Run code from a particular day

--- main.go
+++ main.go
@@ -1,7 +1,18 @@
 package main
 
-import "fmt"
+import (
+	"fmt"
+	"os"
+)
 
 func main() {
-	fmt.Println("Hello, World!")
+	args := os.Args[1:]
+
+	if len(args) > 0 && args[0] == "--" {
+		args = args[1:]
+	}
+	if len(args) < 1 {
+		fmt.Fprintf(os.Stderr, "Usage: ./run <day>\n")
+		os.Exit(1)
+	}
 }
