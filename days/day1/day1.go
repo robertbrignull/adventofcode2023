e032ed7b551da26097ba7aebd744aa5a90b9758f
Robert Brignull <robertbrignull@gmail.com>
2023-12-02 10:46:44 +0000

    Implement day 1 part 1

---
+++ days/day1/day1.go
@@ -0,0 +1,54 @@
+package Day1
+
+import (
+	"fmt"
+	Shared "robertbrignull/adventofcode2023/shared"
+	"strconv"
+)
+
+func isDigit(ch byte) bool {
+	return ch >= '0' && ch <= '9'
+}
+
+func firstNumber(line string) (int, error) {
+	for i := range line {
+		if isDigit(line[i]) {
+			return strconv.Atoi(line[i : i+1])
+		}
+	}
+	return 0, fmt.Errorf("string does not contain any digits: %s", line)
+}
+
+func lastNumber(line string) (int, error) {
+	for j := range line {
+		i := len(line) - 1 - j
+		if isDigit(line[i]) {
+			return strconv.Atoi(line[i : i+1])
+		}
+	}
+	return 0, fmt.Errorf("string does not contain any digits: %s", line)
+}
+
+func Run() (string, error) {
+	lines, err := Shared.ReadFileLines("days/day1/input1.txt")
+	if err != nil {
+		return "", err
+	}
+
+	sum := 0
+	for _, line := range lines {
+		f, err := firstNumber(line)
+		if err != nil {
+			return "", err
+		}
+
+		l, err := lastNumber(line)
+		if err != nil {
+			return "", err
+		}
+
+		sum += f*10 + l
+	}
+
+	return strconv.Itoa(sum), nil
+}
--- main.go
+++ main.go
@@ -2,7 +2,9 @@ package main
 
 import (
 	"fmt"
+	"log"
 	"os"
+	Day1 "robertbrignull/adventofcode2023/days/day1"
 )
 
 func main() {
@@ -12,7 +14,22 @@ func main() {
 		args = args[1:]
 	}
 	if len(args) < 1 {
-		fmt.Fprintf(os.Stderr, "Usage: ./run <day>\n")
-		os.Exit(1)
+		log.Fatal("Usage: ./run <day>\n")
 	}
+
+	var result string
+	var err error
+	result, err = "", nil
+	switch args[0] {
+	case "1":
+		result, err = Day1.Run()
+	default:
+		err = fmt.Errorf("Unrecognised day: %s", args[0])
+	}
+
+	if err != nil {
+		log.Fatalf("%s\n", err)
+	}
+
+	fmt.Printf("%s\n", result)
 }
